cd1393956ef7aba2c66ce3ad133f8f16bf06c302
Naloween <40539093+Naloween@users.noreply.github.com>
2024-01-02 02:34:17 +0100

    hello world

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable all Rust-level entry points
+
+mod vga_buffer;
+
+use core::panic::PanicInfo;
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    vga_buffer::setPrintColor(vga_buffer::ColorCode::new(
+        vga_buffer::Color::LightRed,
+        vga_buffer::Color::Black,
+    ));
+    println!("{}", info);
+    loop {}
+}
+
+#[no_mangle] // don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
+    // this function is the entry point, since the linker looks for a function
+    // named `_start` by default
+
+    println!("Hello World {}", "!");
+
+    panic!("Some panic message");
+
+    // loop {}
+}
